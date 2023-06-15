49d5764fbc7c51f893e5d5aaf47516c64fc8b008
shannon horn <shannonahorn@icloud.com>
2023-06-15 01:24:21 +0000

    api update

--- pages/search.js
+++ pages/search.js
@@ -10,6 +10,9 @@ export default function Search() {
     e.preventDefault();
     const res = await fetch(`/api/search?query=${query}`);
     const data = await res.json();
+
+    console.log(data);
+    
     setResults(data.webPages.value);
   };
 
@@ -24,7 +27,7 @@ export default function Search() {
           onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit" className={styles.searchButton}>
-          <svg viewBox="0 0 24 24" class='icon' width='24' height='24'>
+          <svg viewBox="0 0 24 24" className='icon' width='24' height='24'>
           <path d="M19.25 19.25L15.5 15.5M4.75 11C4.75 7.54822 7.54822 4.75 11 4.75C14.4518 4.75 17.25 7.54822 17.25 11C17.25 14.4518 14.4518 17.25 11 17.25C7.54822 17.25 4.75 14.4518 4.75 11Z"></path>
         </svg>
         </button>
